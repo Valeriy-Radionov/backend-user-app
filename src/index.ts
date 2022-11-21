d53bc59a7f1b6f5252e6a5d85e28533c4f7f82d5
valera <traberker901@gmail.com>
2022-11-21 03:38:08 +0300

    features: port for mongoDB

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { authRouter } from "./rotes/auth-router"
 import { usersRouter } from "./rotes/users-router"
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000 || "mongodb+srv://valery1993:8146189Balrek@usertable.3bgeg4p.mongodb.net/?retryWrites=true&w=majority"
 const parser = express.json()
-const cors = require("cors")
+// const cors = require("cors")
 
-app.use(cors())
+// app.use(cors())
 app.use(parser)
 app.use("/auth", authRouter)
 app.use("/users", usersRouter)
