d7c286b86e49a48b56b30da1a10642847577a145
valera <traberker901@gmail.com>
2022-11-21 15:06:21 +0300

    bugFix: envPORT fix

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
+import cors from "cors"
 import express from "express"
 import { runDb } from "./common/database/userDatabase"
 import { authRouter } from "./rotes/auth-router"
 import { usersRouter } from "./rotes/users-router"
 
 const app = express()
-const port = process.env.PORT || 5000 || "mongodb+srv://valery1993:8146189Balrek@usertable.3bgeg4p.mongodb.net/?retryWrites=true&w=majority"
-const parser = express.json()
-// const cors = require("cors")
+const port = process.env.PORT || 5000
 
-// app.use(cors())
+const parser = express.json()
+app.use(cors())
 app.use(parser)
 app.use("/auth", authRouter)
 app.use("/users", usersRouter)
 const startApp = async () => {
-  await runDb()
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  try {
+    await runDb()
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 startApp()
